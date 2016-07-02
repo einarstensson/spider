ce616b2511d7bdd33873bc8c55c1db01e4b9e0bd
einarstensson <einar.stensson@gmail.com>
2016-07-02 17:37:55 -0400

    First working version

--- index.php
+++ index.php
@@ -2,10 +2,55 @@
   include 'models/vertice.php';
   include 'services/scraper.php';
 
-  $root_url = "http://dn.se";
+  $root_url = "http://wiprodigital.com/";
+  $stack = array();
+  $history = array();
 
-  $vertice = new Vertice( "/" );
   $scraper = new Scraper($root_url);
 
-  $vertice->find_neighbors($scraper);
+  $current_vertice = new Vertice( "/" );
+  $current_vertice->find_neighbors($scraper);
+  $stack[] = $current_vertice;
+
+  $search = true;
+  while($search){
+    $history[] = $current_vertice->name;
+    echo "<br>--------------<br>";
+    echo $current_vertice->name;
+    echo "<br>";
+    $stack = update_stack($current_vertice->neighbors, $stack, $history);
+    array_shift($stack);
+
+    if(count($stack) == 0){
+      $search = false;
+      echo "Done!";
+    }else{
+
+      $current_vertice = $stack[0];
+      $current_vertice->find_neighbors($scraper);
+    }
+  }
+
+  function update_stack($new_neighbors, $stack, $history){
+    if($new_neighbors == NULL){
+      return $stack;
+    }
+
+    foreach($new_neighbors as $neighbor){
+      if(!in_array($neighbor->name, $history)){
+        echo "Neighbor:";
+        echo $neighbor->name;
+        echo "<br>";
+        echo "History:<br>";
+        foreach($history as $name){
+          echo $name;
+          echo "<br>";
+        }
+
+        $stack[] = $neighbor;
+      }
+    }
+
+    return $stack;
+  }
 ?>
--- models/vertice.php
+++ models/vertice.php
@@ -1,7 +1,6 @@
 <?php
   class Vertice {
       public $name = '';
-      public $visited = false;
       public $neighbors = array();
 
       public function __construct( $name ) {
@@ -17,14 +16,10 @@
 
       private function populate_neighbors($fetched_links){
         foreach($fetched_links as $link){
-          if($link){
+          if($link && $link != $this->name){
             $neighbor = new Vertice($link, $this->root_url);
             $this->neighbors[] = $neighbor;
           }
-        }
-
-        foreach($this->neighbors as $neighbor){
-          echo $neighbor->name . "<br>";
         }
       }
   }
